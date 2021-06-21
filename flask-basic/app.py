de8f086fada6346874b2e6a2900d053363c6d671
Rehan Shah <rehan@Rehans-Air.attlocal.net>
2021-06-21 16:26:51 -0700

    Changed gitignore

--- flask-basic/app.py
+++ flask-basic/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from api.base import base_api
 from api.aggregate import aggregate_api
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(base_api, url_prefix='/api')
 app.register_blueprint(aggregate_api, url_prefix='/api/aggregate')
